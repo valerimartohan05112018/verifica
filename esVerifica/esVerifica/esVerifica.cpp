641e45937fb93b6277de14506cbda52404134270
valerimartohan05112018 <valerimartohan05112018@gmail.com>
2018-12-10 09:03:20 +0100

    c=0

---
+++ esVerifica/esVerifica/esVerifica.cpp
@@ -0,0 +1,25 @@
+// esVerifica.cpp: definisce il punto di ingresso dell'applicazione console.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+/*
+Pubblicare sul proprio account GITHUB un repository generato localmente che contenga un 
+file main.cpp ( un programma C che esegue un ciclo da 1 a 10 e visualizzi in output la 
+variabile contatore ) . Il repository dovrà contener il branch "FIX_BUG" oltre al master e 
+dovrà avere più commit su entrambi i rami del repository. Alla fine dovrà essere fatto il 
+merge sul master e generare un conflitto tra i due rami ( mantenere il conflitto nel commit finale ).
+Inserire nella risposta il link al repository ed aggiungermi ( gianlucabraccini) come collaboratore.
+*/
+
+int main()
+{
+	int c = 0;
+	for (int i = 1; i <= 10; i++) {
+		cout << c++;
+	}
+    return 0;
+}
+
